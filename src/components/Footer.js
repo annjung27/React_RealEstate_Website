b28e137fd3769a3bcccfe0cb8759cc7f1ab5bd5a
Annie Jung <younjujung27@gmail.com>
2022-07-25 17:56:52 -0700

    change links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,91 +1,96 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Footer.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
 
 function Footer() {
-    return (
-        <div className='footer-container'>            
-            <div className='footer-links'>                
-                <div className='footer-link-wrapper'>
-                    <div className='footer-link-items'>
-                        <h2>About Us</h2>
-                        <Link to='/sign-up'>How it works</Link>
-                        <Link to='/'>Testimonials</Link>
-                        <Link to='/'>Careers</Link>
-                        <Link to='/'>Investors</Link>
-                        <Link to='/'>Terms of Service</Link>
-                    </div>
-                    <div className='footer-link-items'>
-                        <h2>Contact Us</h2>
-                        <Link to='/'>Contact</Link>
-                        <Link to='/'>Support</Link>                        
-                        <Link to='/'>Sponsorships</Link>
-                    </div>
-                </div>
-                <div className='footer-link-wrapper'>                    
-                    <div className='footer-link-items'>
-                        <h2>Social Media</h2>
-                        <Link to='/'>Instagram</Link>
-                        <Link to='/'>Facebook</Link>
-                        <Link to='/'>Youtube</Link>
-                        <Link to='/'>Twitter</Link>
-                    </div>
-                </div>
-            </div>
-            <section className='social-media'>
-                <div className='social-media-wrap'>
-                    <div className='footer-logo'>
-                        <Link to='/' className='social-logo'>
-                            ALEXANDER REALTY
-                        </Link>
-                    </div>
-                    <small className='rights'>ALEXANDER REALTY @ 2022</small>
-                    <div className='social-icons'>
-                        <Link
-                        className='social-icon-link facebook'
-                        to='/'
-                        target='_blank'
-                        aria-label='Facebook'
-                        >
-                        <i className='fab fa-facebook-f' />
-                        </Link>
-                        <Link
-                        className='social-icon-link instagram'
-                        to='/'
-                        target='_blank'
-                        aria-label='Instagram'
-                        >
-                        <i className='fab fa-instagram' />
-                        </Link>
-                        <Link
-                        className='social-icon-link youtube'
-                        to='/'
-                        target='_blank'
-                        aria-label='Youtube'
-                        >
-                        <i className='fab fa-youtube' />
-                        </Link>
-                        <Link
-                        className='social-icon-link twitter'
-                        to='/'
-                        target='_blank'
-                        aria-label='Twitter'
-                        >
-                        <i className='fab fa-twitter' />
-                        </Link>
-                        <Link
-                        className='social-icon-link twitter'
-                        to='/'
-                        target='_blank'
-                        aria-label='LinkedIn'
-                        >
-                        <i className='fab fa-linkedin' />
-                        </Link>
-                    </div>
-                </div>
-            </section>
+  return (
+    <div className="footer-container">
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+          <div className="footer-link-items">
+            <h2>Contact Us</h2>
+            <Link to="/">Contact</Link>
+            <Link to="/">Support</Link>
+            <Link to="/">Sponsorships</Link>
+          </div>
         </div>
-    )
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>Social Media</h2>
+            <Link to="/">Instagram</Link>
+            <Link to="/">Facebook</Link>
+            <Link to="/">Youtube</Link>
+            <Link to="/">Twitter</Link>
+          </div>
+        </div>
+      </div>
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
+              ALEXANDER REALTY
+            </Link>
+          </div>
+          <small className="rights">ALEXANDER REALTY @ 2022</small>
+          <div className="social-icons">
+            <a
+              className="social-icon-link facebook"
+              href="https://www.facebook.com/"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="Facebook"
+            >
+              <i className="fab fa-facebook-f" />
+            </a>
+            <a
+              className="social-icon-link instagram"
+              href="https://www.instagram.com/"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="Instagram"
+            >
+              <i className="fab fa-instagram" />
+            </a>
+            <a
+              className="social-icon-link youtube"
+              href="https://www.youtube.com/"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="Youtube"
+            >
+              <i className="fab fa-youtube" />
+            </a>
+            <a
+              className="social-icon-link twitter"
+              href="https://twitter.com/?lang=en"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="Twitter"
+            >
+              <i className="fab fa-twitter" />
+            </a>
+            <a
+              className="social-icon-link twitter"
+              href="https://www.linkedin.com/"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="LinkedIn"
+            >
+              <i className="fab fa-linkedin" />
+            </a>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
