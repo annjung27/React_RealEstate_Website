fd82621e8b910ff5112b7d7a7d23310d83797bda
Annie Jung <younjujung27@gmail.com>
2022-07-27 13:54:10 -0700

    change alt

--- src/components/ListingItem.js
+++ src/components/ListingItem.js
@@ -1,24 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function ListingItem(props) {
   return (
     <div>
-        <li className='listings__item'>
-            <Link className='listings__item__link' to={props.path}>
-                <figure className='listings__item__pic-wrap' data-category={props.label}>
-                    <img 
-                    src={props.src} alt='travel'
-                    className='listings__item__img' />
-                </figure>
-                <div className='listings__item__info'>
-                    <h5 className='listings__item__text'>{props.text}</h5>
-                    <p className='listings__item__price'>{props.price}</p>
-                </div>
-            </Link>
-        </li>
+      <li className="listings__item">
+        <Link className="listings__item__link" to={props.path}>
+          <figure
+            className="listings__item__pic-wrap"
+            data-category={props.label}
+          >
+            <img
+              src={props.src}
+              alt="listing-item"
+              className="listings__item__img"
+            />
+          </figure>
+          <div className="listings__item__info">
+            <h5 className="listings__item__text">{props.text}</h5>
+            <p className="listings__item__price">{props.price}</p>
+          </div>
+        </Link>
+      </li>
     </div>
-  )
+  );
 }
 
-export default ListingItem
+export default ListingItem;
